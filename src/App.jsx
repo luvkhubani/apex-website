7b17d826ea3c85b50a140b2d6d2a4777324389d5
Luv Khubani <khubani.l@northeastern.edu>
2026-04-15 12:36:47 +0530

    fix: admin routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, ScrollRestoration } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -9,36 +9,29 @@ import Contact from './pages/Contact';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 
-function ScrollToTop() {
-  return null;
-}
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Admin routes - no Navbar/Footer */}
         <Route path="/admin-apex-secret" element={<AdminLogin />} />
         <Route path="/admin-apex-secret/dashboard" element={<AdminDashboard />} />
-
-        {/* Public routes - with Navbar/Footer */}
-        <Route path="/*" element={
-          <div className="flex flex-col min-h-screen font-sans">
-            <Navbar />
-            <main className="flex-1">
-              <Routes>
-                <Route path="/"         element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/about"    element={<About />} />
-                <Route path="/contact"  element={<Contact />} />
-                <Route path="*"         element={<Home />} />
-              </Routes>
-            </main>
-            <Footer />
-            <WhatsAppButton />
-          </div>
-        } />
+        <Route path="/" element={<PublicLayout><Home /></PublicLayout>} />
+        <Route path="/products" element={<PublicLayout><Products /></PublicLayout>} />
+        <Route path="/about" element={<PublicLayout><About /></PublicLayout>} />
+        <Route path="/contact" element={<PublicLayout><Contact /></PublicLayout>} />
+        <Route path="*" element={<PublicLayout><Home /></PublicLayout>} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function PublicLayout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen font-sans">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+      <WhatsAppButton />
+    </div>
   );
 }
